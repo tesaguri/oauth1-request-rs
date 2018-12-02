0cdb3ae5bf8c7234613351e760f759ecf5ec4c4e
Daiki Mizukami <tesaguriguma@gmail.com>
2018-12-03 03:30:46 +0900

    Tweak compiletest

--- oauth1-request-derive/tests/compiletest.rs
+++ oauth1-request-derive/tests/compiletest.rs
@@ -1,5 +1,6 @@
 extern crate compiletest_rs as compiletest;
 
+use std::fs;
 use std::process::Command;
 
 fn run_mode(mode: &'static str) {
@@ -8,6 +9,13 @@ fn run_mode(mode: &'static str) {
     config.mode = mode.parse().expect("invalid mode");
     config.target_rustcflags = Some("-L test-deps/target/debug/deps".to_owned());
     config.src_base = format!("tests/{}", mode).into();
+
+    for entry in fs::read_dir("test-deps/target/debug/deps").unwrap() {
+        let path = entry.unwrap().path();
+        if path.file_name().unwrap().to_string_lossy().starts_with("liboauth1_request-") {
+            fs::remove_file(path).unwrap();
+        }
+    }
 
     let status = Command::new("cargo")
         .arg("build")
