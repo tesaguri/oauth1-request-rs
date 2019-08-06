3785948ac99f713685f43c9f9c2c5074610db4fa
Daiki Mizukami <tesaguriguma@gmail.com>
2019-08-07 00:18:54 +0900

    Bump to v0.2.3

--- oauth1-request/src/lib.rs
+++ oauth1-request/src/lib.rs
@@ -114,7 +114,7 @@
 //!
 //! See [`Signer`](struct.Signer.html).
 
-#![doc(html_root_url = "https://docs.rs/oauth1-request/0.2.2")]
+#![doc(html_root_url = "https://docs.rs/oauth1-request/0.2.3")]
 
 extern crate base64;
 #[macro_use]
