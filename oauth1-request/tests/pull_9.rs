381e9f65270c591212c691161c8a1899290ad3f5
Daiki Mizukami <tesaguriguma@gmail.com>
2021-10-06 21:10:50 +0900

    Add tests for #9
    <https://github.com/tesaguri/oauth1-request-rs/pull/9>

---
+++ oauth1-request/tests/pull_9.rs
@@ -0,0 +1,14 @@
+//! Test for <https://github.com/tesaguri/oauth1-request-rs/pull/9>.
+
+#[cfg(all(target_arch = "wasm32", target_os = "unknown"))]
+use wasm_bindgen_test::wasm_bindgen_test as test;
+
+#[test]
+fn pull_9() {
+    let _ = oauth1_request::get(
+        "",
+        &(),
+        &oauth_credentials::Token::from_parts("", "", "", ""),
+        oauth1_request::HmacSha1,
+    );
+}
